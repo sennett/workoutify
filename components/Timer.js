5bf74c112d690325c205b557da7a1fd7dab0390d
Anthony Sennett <anthony.sennett@gmail.com>
2016-03-27 21:49:08 +0200

    displaying minutes and seconds

--- components/Timer.js
+++ components/Timer.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const Timer = ({timer}) => (
-  <span>{timer}</span>
-)
+const padWithZeros = (thingToPad) => {
+  thingToPad = thingToPad.toString()
+  if (thingToPad.length >= 2)
+    return thingToPad
+  if (thingToPad.length === 1)
+    return `0${thingToPad}`
+  if (thingToPad.length === 0)
+    return '00'
+}
+
+// timer is in milliseconds
+const Timer = ({timer}) => {
+  let timeInSeconds = timer / 1000
+  let minutes = Math.floor(timeInSeconds / 60)
+  let seconds = Math.round(timeInSeconds % 60)
+  return <span>{padWithZeros(minutes)}:{padWithZeros(seconds)}</span>
+}
 
 export default Timer
