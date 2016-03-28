1927e10d0f78bca5cffb98a7d7dfa352ad7cbf38
Anthony Sennett <anthony.sennett@gmail.com>
2016-03-28 02:08:35 +0200

    nice control icons

---
+++ components/Control.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classNames from 'classnames'
+
+const Control = ({type}) => {
+  let controlClass = classNames('fa', 'fa-stack-1x', 'fa-inverse', type)
+  return (
+    <div className="fa-stack  fa-lg">
+      <div className="fa  fa-circle  fa-stack-2x"></div>
+      <div className={controlClass}></div>
+    </div>
+  )
+}
+
+Control.types = {
+  PLAY: 'fa-play',
+  PAUSE: 'fa-pause',
+  RESET: 'fa-fast-backward'
+}
+
+export default Control
--- containers/Controls.js
+++ containers/Controls.js
@@ -1,12 +1,19 @@
 import { connect } from 'react-redux'
 import React from 'react'
 import { start, pause, reset } from '../actions/controls'
+import Control from '../components/Control'
 
 let Controls = ({start, pause, reset}) => (
-  <div>
-    <button onClick={start}>Start</button>
-    <button onClick={pause}>Pause</button>
-    <button onClick={reset}>Reset</button>
+  <div className="c-controls  o-vertically-centered">
+    <div onClick={start} className="c-control  o-vertically-centered__content">
+      <Control type={Control.types.PLAY} />
+    </div>
+    <div onClick={pause} className="c-control  o-vertically-centered__content">
+      <Control type={Control.types.PAUSE} />
+    </div>
+    <div onClick={reset} className="c-control  o-vertically-centered__content">
+      <Control type={Control.types.RESET} />
+    </div>
   </div>
 )
 
