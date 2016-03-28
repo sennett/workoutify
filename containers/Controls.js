44a1b0c1f57cb811b202c407e4e0517955527f66
Anthony Sennett <anthony.sennett@gmail.com>
2016-03-28 02:20:38 +0200

    disabling controls

--- containers/Controls.js
+++ containers/Controls.js
@@ -2,33 +2,35 @@ import { connect } from 'react-redux'
 import React from 'react'
 import { start, pause, reset } from '../actions/controls'
 import Control from '../components/Control'
+import classNames from 'classnames'
 
-let Controls = ({start, pause, reset}) => (
-  <div className="c-controls  o-vertically-centered">
-    <div onClick={start} className="c-control  o-vertically-centered__content">
-      <Control type={Control.types.PLAY} />
+let Controls = ({start, pause, reset, startEnabled, pauseEnabled}) => {
+  let startClasses = classNames('c-control  o-vertically-centered__content', {
+    'is-enabled': startEnabled
+  })
+  let pauseClasses = classNames('c-control  o-vertically-centered__content', {
+    'is-enabled': pauseEnabled
+  })
+  return (
+    <div className="c-controls  o-vertically-centered">
+      <div onClick={start} className={startClasses}>
+        <Control type={Control.types.PLAY} />
+      </div>
+      <div onClick={pause} className={pauseClasses}>
+        <Control type={Control.types.PAUSE} />
+      </div>
+      <div onClick={reset} className="c-control  o-vertically-centered__content  is-enabled">
+        <Control type={Control.types.RESET} />
+      </div>
     </div>
-    <div onClick={pause} className="c-control  o-vertically-centered__content">
-      <Control type={Control.types.PAUSE} />
-    </div>
-    <div onClick={reset} className="c-control  o-vertically-centered__content">
-      <Control type={Control.types.RESET} />
-    </div>
-  </div>
-)
+  )
+}
 
-const mapStateToProps = (state, ownProps) => state
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  start: () => {
-    dispatch(start())
-  },
-  pause: () => {
-    dispatch(pause())
-  },
-  reset: () => {
-    dispatch(reset())
-  }
+const mapStateToProps = (state, ownProps) => ({
+  startEnabled: state.controls !== 'STARTED',
+  pauseEnabled: state.controls !== 'PAUSED'
 })
+const mapDispatchToProps = { start, pause, reset }
 
 Controls = connect(mapStateToProps, mapDispatchToProps)(Controls)
 
